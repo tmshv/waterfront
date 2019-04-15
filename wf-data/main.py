ad247b9d3959c30a4dfec68872c45f3f3f8adda1
Roman Timashev <roman@tmshv.ru>
2019-04-15 20:29:06 +0300

    wf-data template

---
+++ wf-data/main.py
@@ -0,0 +1,26 @@
+import aiohttp
+
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+
+@app.route('/')
+async def test(request):
+    return json({"hello": "world"})
+
+
+@app.route('/features')
+async def test(request):
+    url = 'https://wf.tmshv.com/api/_/items/features'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            data = await resp.json()
+
+            return json(data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
