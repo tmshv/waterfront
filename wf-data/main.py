867e403e9ba6973d8872c9c67361c454bab82ffd
Roman Timashev <roman@tmshv.ru>
2019-05-22 18:40:59 +0300

    wf-data get single feature

--- wf-data/main.py
+++ wf-data/main.py
@@ -20,6 +20,18 @@ async def api_features():
             return data['data']
 
 
+async def api_feature_by_slug(slug):
+    url = api_url(f'/api/_/items/features?filter[slug][eq]={slug}')
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            res_data = await resp.json()
+            data = res_data['data']
+            if len(data) == 0:
+                return None
+            return data[0]
+
+
 async def api_image(image_id):
     url = api_url(f'/api/_/files/{image_id}')
 
@@ -58,6 +70,80 @@ async def home(request):
             'city': ['saint_petersburg', 'shockholm', '__any__'],
             'status': ['published', 'draft'],
         }
+    })
+
+
+@app.route('/<lang>/features/<slug>')
+@cross_origin(app)
+async def get_feature(request, lang, slug):
+    """
+    API Item structure
+    {
+      "id": 1,
+      "status": "published",
+      "created_by": 1,
+      "created_on": "2019-04-14 18:34:35",
+      "location": {
+        "lat": 59.92663548229322,
+        "lng": 30.23119926452637
+      },
+      "name_ru": "WF",
+      "name_en": "WF",
+      "content_ru": "content",
+      "content_en": "content",
+      "actor_type": "artist",
+      "project_type": "art_intervention",
+      "preview_image": 6
+    }
+    :param request:
+    :param lang:
+    :param city:
+    :param status:
+    :return:
+    """
+    name_field = f'name_{lang}'
+    short_field = f'short_{lang}'
+    content_field = f'content_{lang}'
+
+    item = await api_feature_by_slug(slug)
+
+    if not item:
+        return json({
+            'error': 'not found'
+        })
+
+    if item['preview_image']:
+        image_data = await api_image(item['preview_image'])
+        image = create_image_url(image_data)
+    else:
+        image = None
+
+    properties = {
+        'city': item['city'],
+        'actorType': item['actor_type'],
+        'slug': item['slug'],
+        'year': item['year'],
+        # 'placementtype': item['placement_type'],
+        'projectType': item['project_type'],
+        'name': item[name_field],
+        'short': item[short_field],
+        'content': item[content_field],
+        'previewImage': image,
+    }
+
+    return json({
+        'type': 'FeatureCollection',
+        'features': [{
+            'type': 'Feature',
+            'properties': clean(properties),
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [
+                    item['location']['lng'],
+                    item['location']['lat'],
+                ]
+            }
+        }],
     })
 
 
