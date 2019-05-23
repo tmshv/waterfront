fe94d620a4f191ecadfd11d081c066767c5bdd42
Roman Timashev <roman@tmshv.ru>
2019-05-23 22:15:40 +0300

    wf-app factory update

--- wf-app/src/app/factory.js
+++ wf-app/src/app/factory.js
@@ -1,6 +1,12 @@
+import { renderMarkdown } from '../lib'
+
 export function toArticle(feature) {
+    const short = feature.properties.short
+
     return {
         ...feature.properties,
+        short: short ? renderMarkdown(short) : '',
+        name: renderMarkdown(feature.properties.name),
         body: feature.properties.content,
         url: `/project/${feature.properties.slug}`,
     }
