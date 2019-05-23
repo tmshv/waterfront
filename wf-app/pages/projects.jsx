6c89fc1b5701d58b2c8f9641024794cdecb4dd75
Roman Timashev <roman@tmshv.ru>
2019-05-23 20:23:24 +0300

    wf-app projects layout added

--- wf-app/pages/projects.jsx
+++ wf-app/pages/projects.jsx
@@ -2,19 +2,31 @@ import React, { useReducer } from 'react'
 import { getFeatures } from '../src/api'
 import { toArticle } from '../src/app/factory'
 import ArticleCard from '../src/components/ArticleCard'
+import Menu from '../src/components/Menu'
+import Footer from '../src/components/Footer'
+import DefaultLayout from '../src/components/DefaultLayout'
 
-const Projects = (props) => {
-    return (
-        <div>
-            {props.articleCards.map(x => (
-                <ArticleCard
-                    key={x.slug}
-                    article={x}
-                />
-            ))}
-        </div>
-    )
-}
+const Projects = (props) => (
+    <DefaultLayout
+        headerOverlay={true}
+        header={(
+            <Menu />
+        )}
+        main={(
+            <div>
+                {props.articleCards.map(x => (
+                    <ArticleCard
+                        key={x.slug}
+                        article={x}
+                    />
+                ))}
+            </div>
+        )}
+        footer={(
+            <Footer />
+        )}
+    />
+)
 
 Projects.getInitialProps = async ({ query }) => {
     // const city = query.city || 'saint_peterburg'
