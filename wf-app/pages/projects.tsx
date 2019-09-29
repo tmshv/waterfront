2f3e37a1de1aabcabef33ed9bfb9ef8de68814b2
Roman Timashev <roman@tmshv.ru>
2019-09-29 23:24:20 +0300

    projects page refactorig + translation

--- wf-app/pages/projects.tsx
+++ wf-app/pages/projects.tsx
@@ -1,13 +1,22 @@
-import React, { useReducer } from 'react'
+import * as React from 'react'
+
+import { NextPage } from 'next'
+
 import { getFeatures } from '../src/api'
 import { toArticle } from '../src/app/factory'
 import { ArticleCardList } from '../src/components/ArticleCardList'
 import { Menu } from '../src/components/Menu'
 import Footer from '../src/components/Footer'
 import { DefaultLayout } from '../src/components/DefaultLayout'
+import { AppPointFeature } from '../src/app/types'
+import { i18n } from '../src/i18n'
 
-const Projects = (props) => {
-    const articleCards = props.data.features.map(toArticle)
+interface IProps {
+    features: AppPointFeature[]
+}
+
+export const Page: NextPage<IProps> = props => {
+    const articleCards = props.features.map(toArticle)
 
     return (
         <DefaultLayout
@@ -37,14 +46,20 @@ const Projects = (props) => {
     )
 }
 
-Projects.getInitialProps = async ({ query }) => {
-    // const city = query.city || 'saint_peterburg'
+Page.getInitialProps = async ({ req }) => {
+    let lang: string | null = null
+    if (req) {
+        lang = (req as any).i18n.language
+    } else {
+        lang = i18n.language
+    }
+
     const city = 'saint_petersburg'
-    const data = await getFeatures('en', city, true)
+    const data = await getFeatures(lang!, city, true)
 
     return {
-        data,
+        features: data,
     }
 }
 
-export default Projects
+export default Page
