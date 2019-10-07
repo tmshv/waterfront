bf660070ed96543d1bc755e5ac9d52383fc277a6
Roman Timashev <roman@tmshv.ru>
2019-10-07 21:46:43 +0300

    about uses PageLayout instead of DefaultLayout

--- wf-app/pages/about.tsx
+++ wf-app/pages/about.tsx
@@ -13,6 +13,7 @@ import { IArticle, IPerson } from '../src/app/types'
 import { Header } from '../src/components/Header'
 import { PersonsBlock } from '../src/components/PersonsBlock'
 import { PageHead } from '../src/components/PageHead'
+import { PageLayout } from '../src/components/PageLayout'
 
 interface IProps {
     article: IArticle
@@ -25,48 +26,32 @@ const Page: NextPage<IProps> = props => {
     const layout = useLayout()
 
     return (
-        <DefaultLayout
-            navigation={(
-                <Header
-                    layout={layout}
-                >
-                    <Menu
-                        layout={layout}
-                    />
-                </Header>
-            )}
-            head={(
-                <PageHead
-                    title={props.article.name}
-                    caption={props.article.short}
-                    image={props.article.previewImage}
-                />
-            )}
-            main={(
-                <>
-                    <Article
-                        article={props.article}
-                    />
-                    <PersonsBlock
-                        title={'Team'}
-                        items={props.team}
-                    />
+        <PageLayout
+            head={{
+                title: props.article.name,
+                caption: props.article.short,
+                image: props.article.previewImage,
+            }}
+        >
+            <Article
+                article={props.article}
+            />
 
-                    <PersonsBlock
-                        title={'Experts'}
-                        items={props.team}
-                    />
+            <PersonsBlock
+                title={'Team'}
+                items={props.team}
+            />
 
-                    <PersonsBlock
-                        title={'Partners'}
-                        items={props.team}
-                    />
-                </>
-            )}
-            footer={(
-                <Footer />
-            )}
-        />
+            <PersonsBlock
+                title={'Experts'}
+                items={props.team}
+            />
+
+            <PersonsBlock
+                title={'Partners'}
+                items={props.team}
+            />
+        </PageLayout>
     )
 }
 
