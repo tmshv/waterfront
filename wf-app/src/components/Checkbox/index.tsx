c22064462fed6b76cd33e2d8965b672dbd4bfc6b
Roman Timashev <roman@tmshv.ru>
2019-10-09 18:11:38 +0300

    Checkbox custom style removed

--- wf-app/src/components/Checkbox/index.tsx
+++ wf-app/src/components/Checkbox/index.tsx
@@ -9,45 +9,17 @@ export interface ICheckboxProps {
 export const Checkbox: React.FC<ICheckboxProps> = props => (
     <label>
         <style jsx>{`
-                label {
-                    flex; 1;
-                    cursor: pointer;
-
-                    padding-left: 1.0em;
-                }
-
-                input {
-                    position: absolute;
-                    appearance: none;
-                }
-
-                input:checked + span {
-                    background-color: black;
-                    box-shadow: 0 0 0 2px white inset;
-                }
-                
-                input:focus + span {
-                    box-shadow: 0 0 0 2px white;
-                }
-
-                span {
-                    position: absolute;
-                    margin-top: 3px;
-                    margin-left: -1.0em;
-                    width: 10px;
-                    height: 10px;
-                    overflow: hidden;
-                    background-color: white;
-                }
-            `}</style>
+            label {
+                flex; 1;
+                cursor: pointer;
+            }
+        `}</style>
 
         <input
             type={'checkbox'}
             checked={props.checked}
             onChange={props.onChange}
         />
-
-        <span />
 
         {props.label}
     </label>
