1437f14a608fa787843ec548fbcefc058ee2d549
Roman Timashev <roman@tmshv.ru>
2020-02-10 15:27:30 +0300

    app/document refactored

--- wf-app/pages/_app.tsx
+++ wf-app/pages/_app.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import App, { Container } from 'next/app'
-import { appWithTranslation } from '../src/i18n'
+import App from 'next/app'
+import { Head } from 'next/document'
+import { appWithTranslation } from 'src/i18n'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
-import '../src/style.css'
+import 'src/style.css'
 
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props
 
         return (
-            <Container>
-                <Component {...pageProps} />
-            </Container>
+            <Component {...pageProps}>
+                <Head>
+                    <meta name={'viewport'} content={'initial-scale=1,maximum-scale=1,user-scalable=no'} />
+                </Head>
+            </Component>
         )
     }
 }
