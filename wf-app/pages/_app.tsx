7543d1653ff1424287cdd886accf878564dfacb6
Roman Timashev <roman@tmshv.ru>
2020-02-10 15:18:18 +0300

    main style moved to css

--- wf-app/pages/_app.tsx
+++ wf-app/pages/_app.tsx
@@ -3,6 +3,7 @@ import App, { Container } from 'next/app'
 import { appWithTranslation } from '../src/i18n'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
+import '../src/style.css'
 
 class MyApp extends App {
     render() {
@@ -10,27 +11,6 @@ class MyApp extends App {
 
         return (
             <Container>
-                <style global jsx>{`
-                    :root {
-                        --body-background-color: white;
-                        --footer-background-color: rgb(0, 83, 108);
-                        --background-color: rgb(255, 255, 255);
-                    }
-
-                    body {
-                        font-family: Arial;
-                        background-color: var(--body-background-color);
-
-                        height: 100vh;
-                        padding: 0;
-                        margin: 0;
-                    }
-
-                    #__next {
-                        height: 100%;
-                    }
-                `}</style>
-                
                 <Component {...pageProps} />
             </Container>
         )
