ca9e5d24bf1b1d6e2f7b0717108c3b4a1273cf8b
Roman Timashev <roman@tmshv.ru>
2020-04-15 22:58:26 +0300

    temporary disable image resize

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -11,7 +11,8 @@ function isId(value: string | number) {
 }
 
 function resizeImage(src: string, options: IRect): string {
-    return `https://images.weserv.nl/?url=${src}&w=${options.w}&h=${options.h}&n=-1`
+    return src
+    // return `https://images.weserv.nl/?url=${src}&w=${options.w}&h=${options.h}&n=-1`
 }
 
 export function useImage(param: string | number, size: IRect): string | undefined {
