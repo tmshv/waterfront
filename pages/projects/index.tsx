7c0451b0d6fb7571321c59ef290c37de545836a9
Roman Timashev <roman@tmshv.ru>
2020-06-20 01:01:03 +0300

    projects page activate

---
+++ pages/projects/index.tsx
@@ -0,0 +1,42 @@
+import { NextPage, GetStaticProps } from 'next'
+import { useTranslation } from '@/hooks/useTranslation'
+import { useColumns } from '@/hooks/useColumns'
+import { CardsPage } from '@/components/CardsPage'
+import { getProjectCards } from '@/app/catalog'
+import { PageDescription } from '@/types'
+
+interface IProps {
+    pages: PageDescription[]
+}
+
+export const Page: NextPage<IProps> = props => {
+    const { t } = useTranslation()
+    const columns = useColumns()
+
+    return (
+        <CardsPage
+            title={t('Projects')}
+            excerpt={t('Projects')}
+            cover={'/assets/wf_about_head.jpg'}
+            items={props.pages.map(x => ({
+                title: x.title!,
+                cover: x.cover,
+                excerpt: x.excerpt,
+                slug: x.slug,
+                tags: x.tags,
+            }))}
+        />
+    )
+}
+
+export const getStaticProps: GetStaticProps<IProps> = async ({ params }) => {
+    const pages = await getProjectCards('ru')
+
+    return {
+        props: {
+            pages,
+        },
+    }
+}
+
+export default Page
