e45c8b0eca4a444f21374ca5e509528f6d7455d1
Roman Timashev <roman@tmshv.ru>
2020-06-24 00:06:29 +0300

    Menu component update: highlight active item

--- src/components/Menu/lib.ts
+++ src/components/Menu/lib.ts
@@ -3,14 +3,21 @@ import { useContext } from 'react'
 import { MenuContext } from '@/context/menu'
 import { useLanguage } from '@/hooks/useLanguage'
 import { changeLangPathSuffix } from '@/lib/lang'
+import { useRouter } from 'next/router'
 
 export function useMenuItems() {
+    const router = useRouter()
     const { t } = useTranslation()
     const lang = useLanguage()
     const items = useContext(MenuContext)
 
-    return items.map(item => ({
-        href: changeLangPathSuffix(lang, item.url),
-        label: t(item.name),
-    }))
+    return items.map(item => {
+        const href = changeLangPathSuffix(lang, item.url)
+
+        return {
+            href,
+            label: t(item.name),
+            isCurrent: router.asPath === href,
+        }
+    })
 }
