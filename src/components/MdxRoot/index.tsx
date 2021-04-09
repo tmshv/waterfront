49f6bd475500173423aa026fdea130c640d470a7
Roman Timashev <roman@tmshv.ru>
2021-04-09 13:58:37 +0300

    mdx pages uses next image

--- src/components/MdxRoot/index.tsx
+++ src/components/MdxRoot/index.tsx
@@ -5,6 +5,7 @@ import { Person } from '@/components/Person'
 import { WideBlock } from '@/components/WideBlock'
 import { Video } from '@/components/Video'
 import { A } from '@/components/A'
+import Image from 'next/image'
 
 const H1: React.FC<{ children: string }> = props => {
     const config = useContext(PageContext)
@@ -19,6 +20,21 @@ const H1: React.FC<{ children: string }> = props => {
                 image={config.cover}
             />
         </WideBlock>
+    )
+}
+
+const Img: React.FC<{ src: string }> = props => {
+    const config = useContext(PageContext)
+
+    return (
+        <Image
+            src={props.src}
+            alt={config.excerpt}
+            width={4}
+            height={3}
+            layout={'responsive'}
+            objectFit={'cover'}
+        />
     )
 }
 
@@ -37,6 +53,7 @@ export const components = {
     ),
     h1: H1,
     a: A,
+    img: Img,
     Person: (props: PersonData) => (
         <Person
             shape={props.shape ?? 'default'}
